c6665fd666e3be6547d30514cc0cebbcbe8090ec
Ismail Hossain <ismailplusbd1994@gmail.com>
2023-08-08 15:27:56 +0600

    added a default users on migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,6 +22,13 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        DB::table('users')->insert([
+            [
+                'name' => 'Ismail Hossain',
+                'email' => 'ismailreformed@gmail.com',
+                'password' => Hash::make('password'),
+            ],
+        ]);
     }
 
     /**
