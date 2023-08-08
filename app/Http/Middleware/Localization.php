4bc013ada4458523b9c45295ed1c5340c48afbd5
Ismail Hossain <ismailplusbd1994@gmail.com>
2023-08-08 15:22:12 +0600

    added localtization for auth user

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class Localization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth('api')->check()) {
+            $user = auth('api')->user();
+            app()->setLocale($user->local);
+        }
+        return $next($request);
+    }
+}
